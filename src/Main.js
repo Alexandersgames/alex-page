8a0574e4991bf3cfe35bff49033203cde658710f
Alexandersgames <78982281+Alexandersgames@users.noreply.github.com>
2021-07-20 14:28:52 +0200

    Create sidebar

--- src/Main.js
+++ src/Main.js
@@ -3,15 +3,21 @@
 function Main() {
     return (
         <main>
-            <p>Välkommen</p>
-            <ul>
-                <li>
-                    <a href="https://www.youtube.com/channel/UCrSXNPSlbk9E4i3gmClR3Jg">Alexander,s games</a>
-                </li>
-                <li>
-                    <a href="https://www.youtube.com/channel/UCZFtiHgyUJ39_EHOGOuDEzA" >Alexanders Games FNF</a>
-                </li>
-            </ul>
+            <div className="sidebar">
+                <h4>Alex Site</h4>
+            </div>
+            <div className="middle">
+                <p>Välkommen</p>
+                <ul>
+                    <li>
+                        <a href="https://www.youtube.com/channel/UCrSXNPSlbk9E4i3gmClR3Jg">Alexander,s games</a>
+                    </li>
+                    <li>
+                        <a href="https://www.youtube.com/channel/UCZFtiHgyUJ39_EHOGOuDEzA" >Alexanders Games FNF</a>
+                    </li>
+                </ul>
+            </div>
+
 
 
 
