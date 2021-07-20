3178647e281c409b30070fee915b84bca2162c0b
Alexandersgames <78982281+Alexandersgames@users.noreply.github.com>
2021-07-20 13:37:34 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Alex Page</h1>
+          <p>Välkommen</p>
+          <a href="https://www.youtube.com/channel/UCrSXNPSlbk9E4i3gmClR3Jg">min kanal</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
